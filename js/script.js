7832039409a4172bc52b4deda249c051e32ef970
Sebastian Rzeszutko <rzeszutkoseb@gmail.com>
2025-09-03 07:38:03 +0200

    Add var computerMove and playerMove

---
+++ js/script.js
@@ -0,0 +1,6 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
