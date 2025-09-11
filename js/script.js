a5cccb2a9b071705e04782749e86f9bdf4eed121
Sebastian Rzeszutko <rzeszutkoseb@gmail.com>
2025-09-11 06:45:22 +0200

    const for buttonClicked

--- js/script.js
+++ js/script.js
@@ -44,12 +44,12 @@ function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
 
-  var playerMove = argButtonName;
+  const playerMove = argButtonName;
 
  
-  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  var computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
 
 
